264644bac0676f41d78446a80386cb9125a536fe
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-07-18 19:34:43 -0400

    1.1.2 add button function

--- gui.py
+++ gui.py
@@ -11,16 +11,20 @@ aLabel.grid(column=0,row=0)
 #ttk.Label(win,text="A Label").grid(column=0,row=0)
 #win.mainloop()
 
-
 #Button Click Event Function
 
 def clickMe():
-    action.configure(text="** I have been Clocked!")
-    aLabel.configure(foreground="red")
-    aLabel.configure(text="a Red Label")
+    action.configure(text="Hello"+name.get())
 
+#Changing our Label
+ttk.Label(win,text="Enter a name: ").grid(column=0,row=0)
+
+# Adding a Textbox Entry widget
+name = tk.StringVar()
+nameEntered = ttk.Entry(win,width=12,textvariable=name)
+nameEntered.grid(column=0,row=1)
 #Adding button
 action = ttk.Button(win,text="Click Me!",command=clickMe)
-action.grid(column=1,row=0)
+action.grid(column=1,row=1)
 
 win.mainloop()
